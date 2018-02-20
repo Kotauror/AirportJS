2ca46a12344834b09b65481aafb227eaff4e0d6b
kotpsot <zygmunt.justyna@gmail.com>
2018-02-20 23:06:02 +0000

    add unit tests for plane class.

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -8,11 +8,23 @@ describe('Plane',function(){
 
   beforeEach(function(){
     plane = new Plane();
-    airport = jasmine.createSpyObj('airport',['_planes', 'hasPlaces', '_weather']);
-    weather = jasmine.createSpyObj('weather',['isStormy']);
+    airport = jasmine.createSpyObj('airport',['_planes', 'clearLanding', 'clearTakeoff', 'hasPlaces', 'hasPlane' '_weather']);
+    airport._weather.isStormy = function(){return false;}
+    airport.hasPlaces = function(){return true;}
   });
 
   it('has initial status flying', function(){
+    expect(plane.status).toEqual("flying");
+  });
+
+  it('changes the status after landing', function(){
+    plane.land(airport);
+    expect(plane.status).toEqual("at_airport");
+  });
+
+  it('changes the status after takeoff', function(){
+    airport.hasPlane = function(){return true;}
+    plane.takeoff(airport);
     expect(plane.status).toEqual("flying");
   });
 
