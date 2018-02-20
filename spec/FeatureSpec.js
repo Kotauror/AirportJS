21afc6087e3a75cb0ac4cbbe1a0a1408b1562749
kotpsot <zygmunt.justyna@gmail.com>
2018-02-20 17:17:34 +0000

    make before blocks setting the weather.

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -12,80 +12,80 @@ describe('Feature Test:', function() {
     weather = new Weather();
   });
 
-  it('planes can be instructed to land at an airport', function() {
-    spyOn(Math, 'floor').and.returnValue(2);
-    plane.land(airport);
-    expect(airport._planes).toContain(plane);
+  describe('the weather is nice', function() {
+    beforeEach(function() {
+      spyOn(Math, 'floor').and.returnValue(2);
+    });
+
+    it('planes can be instructed to land at an airport', function() {
+      plane.land(airport);
+      expect(airport._planes).toContain(plane);
+    });
+
+    it('planes can be instructed to takeoff from the airport', function() {
+      plane.takeoff(airport);
+      expect(airport._planes).not.toContain(plane);
+    });
+
+    it('initial status of plane is flying', function() {
+      expect(plane.status).toEqual("flying");
+    });
+
+    it('changes the plane status on landing', function() {
+      plane.land(airport);
+      expect(plane.status).toEqual("at_airport");
+    });
+
+    it('changes the plane status on takeoff', function() {
+      plane.land(airport);
+      plane.takeoff(airport);
+      expect(plane.status).toEqual("flying");
+    });
+
+    it('confirms the takeoff', function() {
+      plane.land(airport);
+      plane.takeoff(airport);
+      expect(airport.takeoffConfirm(plane)).toEqual("This plane is not on this airport")
+    });
+
+    it('says the plane is in the airport', function() {
+      plane.land(airport);
+      expect(airport.takeoffConfirm(plane)).toEqual("This plane is still in the airport")
+    });
+
+    it('plane cannot land twice', function () {
+      plane.land(airport);
+      plane.land(airport);
+      expect(airport._planes.length).toEqual(1);
+    });
+
+    it('plane cannot takeoff twice', function () {
+      plane.land(airport);
+      plane.takeoff(airport);
+      plane.takeoff(airport);
+      expect(airport._planes.length).toEqual(0);
+    });
   });
 
-  it('planes can be instructed to takeoff from the airport', function() {
-    spyOn(Math, 'floor').and.returnValue(2);
-    plane.takeoff(airport);
-    expect(airport._planes).not.toContain(plane);
-  });
+  describe('the weather is stormy', function() {
+    beforeEach(function() {
+      spyOn(Math, 'floor').and.returnValue(1);
+    });
 
-  it('initial status of plane is flying', function() {
-    spyOn(Math, 'floor').and.returnValue(2);
-    expect(plane.status).toEqual("flying");
-  });
+    it('weather can be stormy', function () {
+       expect(weather.isStormy()).toBeTruthy();
+    });
 
-  it('changes the plane status on landing', function() {
-    spyOn(Math, 'floor').and.returnValue(2);
-    plane.land(airport);
-    expect(plane.status).toEqual("at_airport");
-  });
+    it('plane cannot land when weather is stormy', function () {
+      plane.land(airport);
+      expect(airport._planes.length).toEqual(0);
+    });
 
-  it('changes the plane status on takeoff', function() {
-    spyOn(Math, 'floor').and.returnValue(2);
-    plane.land(airport);
-    plane.takeoff(airport);
-    expect(plane.status).toEqual("flying");
+    it('plane cannot takeoff when weather is stormy', function () {
+      airport._planes = [plane];
+      plane.takeoff(airport);
+      expect(airport._planes.length).toEqual(1);
+    });
   });
-
-  it('confirms the takeoff', function() {
-    spyOn(Math, 'floor').and.returnValue(2);
-    plane.land(airport);
-    plane.takeoff(airport);
-    expect(airport.takeoffConfirm(plane)).toEqual("This plane is not on this airport")
-  });
-
-  it('says the plane is in the airport', function() {
-    spyOn(Math, 'floor').and.returnValue(2);
-    plane.land(airport);
-    expect(airport.takeoffConfirm(plane)).toEqual("This plane is still in the airport")
-  });
-
-  it('plane cannot land twice', function () {
-    spyOn(Math, 'floor').and.returnValue(2);
-    plane.land(airport);
-    plane.land(airport);
-    expect(airport._planes.length).toEqual(1);
-  });
-
-  it('plane cannot takeoff twice', function () {
-    spyOn(Math, 'floor').and.returnValue(2);
-    plane.land(airport);
-    plane.takeoff(airport);
-    plane.takeoff(airport);
-    expect(airport._planes.length).toEqual(0);
-  });
-
-  it('weather can be stormy', function () {
-     spyOn(Math, 'floor').and.returnValue(1);
-     expect(weather.isStormy()).toBeTruthy();
-  });
-
-  it('plane cannot land when weather is stormy', function () {
-    spyOn(Math, 'floor').and.returnValue(1);
-    plane.land(airport);
-    expect(airport._planes.length).toEqual(0);
-  });
-
-  it('plane cannot takeoff when weather is stormy', function () {
-    spyOn(Math, 'floor').and.returnValue(1);
-    airport._planes = [plane];
-    plane.takeoff(airport);
-    expect(airport._planes.length).toEqual(1);
-  });
-
+  
 });
